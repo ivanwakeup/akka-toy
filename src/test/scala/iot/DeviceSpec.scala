ed8c7cb6a185cba5d1e7c63947dfbc94b5e2f03b
ivanwakeup <ivan9miller@gmail.com>
2018-08-27 12:58:36 -0600

    fix device spec

--- src/test/scala/iot/DeviceSpec.scala
+++ src/test/scala/iot/DeviceSpec.scala
@@ -7,26 +7,45 @@ import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FlatSpecLike, Match
 class DeviceSpec extends TestKit(ActorSystem()) with Matchers with FlatSpecLike with BeforeAndAfterEach with BeforeAndAfterAll {
 
 
-  val probe = TestProbe()
-  val deviceActor = system.actorOf(Device.props("group", "device"))
+
 
   override def beforeEach() = {
-    deviceActor ! PoisonPill
+    //deviceActor ! PoisonPill
   }
 
   override def afterAll() = {
     TestKit.shutdownActorSystem(system)
   }
 
-  "A Device Actor" should "reply with empty reading if no temperature is known"  in {
-    deviceActor.tell(Device.ReadTemperature(requestId = 42), probe.ref)
-    val response = probe.expectMsgType[Device.RespondTemperature]
-    response.requestId should ===(42)
-    response.value should ===(None)
-  }
+  "A Device Actor" should "reply with latest temperature reading" in {
 
-  it should "reply with latest temperature reading" in {
+      val probe = TestProbe()
+      val deviceActor = system.actorOf(Device.props("group", "device"))
 
+      deviceActor.tell(Device.RecordTemperature(requestId = 1, 24.0), probe.ref)
+      probe.expectMsg(Device.TemperatureRecorded(requestId = 1))
+
+      deviceActor.tell(Device.ReadTemperature(requestId = 2), probe.ref)
+      val response1 = probe.expectMsgType[Device.RespondTemperature]
+      response1.requestId should ===(2)
+      response1.value should ===(Some(24.0))
+
+      deviceActor.tell(Device.RecordTemperature(requestId = 3, 55.0), probe.ref)
+      probe.expectMsg(Device.TemperatureRecorded(requestId = 3))
+
+      deviceActor.tell(Device.ReadTemperature(requestId = 4), probe.ref)
+      val response2 = probe.expectMsgType[Device.RespondTemperature]
+      response2.requestId should ===(4)
+      response2.value should ===(Some(55.0))
+
+    }
+
+  it should "reply again" in {
+    val probe = TestProbe()
+    val deviceActor = system.actorOf(Device.props("group", "device"))
+
+    deviceActor.tell(Device.RecordTemperature(requestId = 77, 24.0), probe.ref)
+    probe.expectMsg(Device.TemperatureRecorded(requestId = 77))
   }
 
 }
